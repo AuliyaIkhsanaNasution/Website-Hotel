8d8ac507c7d5e68a843ef331e4c9604547cccbfe
Muhammad Bobby <muhammadbobbyoktaviano@gmail.com>
2024-05-06 10:25:03 +0700

    export database

---
+++ hotel.sql
@@ -0,0 +1,130 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for hotel
+CREATE DATABASE IF NOT EXISTS `hotel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hotel`;
+
+-- Dumping structure for table hotel.detailpemesanan
+CREATE TABLE IF NOT EXISTS `detailpemesanan` (
+  `detail_pemesanan_id` int NOT NULL AUTO_INCREMENT,
+  `pemesanan_id` int DEFAULT NULL,
+  `kamar_id` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tipe_kamar` int NOT NULL,
+  `harga_kamar_per_malam` decimal(10,2) NOT NULL,
+  `fasilitas_plus` enum('Sarapan','Tanpa Sarapan') COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`detail_pemesanan_id`),
+  KEY `pemesanan_id` (`pemesanan_id`),
+  KEY `FK_detailpemesanan_tipekamar` (`tipe_kamar`),
+  KEY `FK_detailpemesanan_kamar` (`kamar_id`),
+  CONSTRAINT `detailpemesanan_ibfk_1` FOREIGN KEY (`pemesanan_id`) REFERENCES `pemesanan` (`pemesanan_id`),
+  CONSTRAINT `FK_detailpemesanan_kamar` FOREIGN KEY (`kamar_id`) REFERENCES `kamar` (`kamar_id`),
+  CONSTRAINT `FK_detailpemesanan_tipekamar` FOREIGN KEY (`tipe_kamar`) REFERENCES `tipekamar` (`tipe_kamar_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table hotel.kamar
+CREATE TABLE IF NOT EXISTS `kamar` (
+  `kamar_id` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `nomor_kamar` int NOT NULL,
+  `tipe_kamar_id` int DEFAULT NULL,
+  `harga_kamar` int NOT NULL,
+  `fasilitas` enum('Tanpa Sarapan','Sarapan','','') COLLATE utf8mb4_general_ci NOT NULL,
+  `status_kamar` enum('tersedia','dipesan','ditempati') COLLATE utf8mb4_general_ci DEFAULT 'tersedia',
+  PRIMARY KEY (`kamar_id`),
+  KEY `tipe_kamar_id` (`tipe_kamar_id`),
+  CONSTRAINT `kamar_ibfk_1` FOREIGN KEY (`tipe_kamar_id`) REFERENCES `tipekamar` (`tipe_kamar_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table hotel.pegawai
+CREATE TABLE IF NOT EXISTS `pegawai` (
+  `pegawai_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `no_telepon` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `posisi_id` int DEFAULT NULL,
+  PRIMARY KEY (`pegawai_id`),
+  KEY `posisi_id` (`posisi_id`),
+  CONSTRAINT `pegawai_ibfk_1` FOREIGN KEY (`posisi_id`) REFERENCES `posisi` (`posisi_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table hotel.pemesanan
+CREATE TABLE IF NOT EXISTS `pemesanan` (
+  `pemesanan_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `tanggal_pesan` date NOT NULL,
+  `tanggal_checkin` date NOT NULL,
+  `tanggal_checkout` date NOT NULL,
+  `jumlah_tamu` int NOT NULL,
+  `total_harga` decimal(10,2) NOT NULL,
+  `status_pemesanan` enum('dikonfirmasi','batal','selesai') COLLATE utf8mb4_general_ci DEFAULT 'dikonfirmasi',
+  PRIMARY KEY (`pemesanan_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table hotel.posisi
+CREATE TABLE IF NOT EXISTS `posisi` (
+  `posisi_id` int NOT NULL AUTO_INCREMENT,
+  `posisi` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`posisi_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table hotel.tipekamar
+CREATE TABLE IF NOT EXISTS `tipekamar` (
+  `tipe_kamar_id` int NOT NULL AUTO_INCREMENT,
+  `slug` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nama_tipe` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `jmlh_tamu` int DEFAULT NULL,
+  `deskripsi` text COLLATE utf8mb4_general_ci,
+  `ukuran` int DEFAULT NULL,
+  `harga` int DEFAULT NULL,
+  `image` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`tipe_kamar_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table hotel.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `no_telepon` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- user/pemesanan.php
+++ user/pemesanan.php
@@ -46,30 +46,30 @@ $typeKamar = mysqli_query($conn, "SELECT * FROM tipeKamar");
     <script src="https://kit.fontawesome.com/22f19496c5.js" crossorigin="anonymous"></script>
 </head>
 
-<body class="overflow-x-hidden bg-slate-100 w-screen">
+<body class="w-screen overflow-x-hidden bg-slate-100">
 
     <!-- navbar -->
     <section>
-        <nav class="bg-blue-800 border-gray-200 w-screen">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <nav class="w-screen bg-blue-800 border-gray-200">
+            <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
                 <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                    <span class="self-center text-lg lg:text-2xl font-bold whitespace-nowrap text-yellow-400">Nuansa Nusantara</span>
+                    <span class="self-center text-lg font-bold text-yellow-400 lg:text-2xl whitespace-nowrap">Nuansa Nusantara</span>
                 </a>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                     </svg>
                 </button>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+                    <ul class="flex flex-col p-4 mt-4 font-medium border md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
                         <li>
-                            <div class="flex lg:order-2 space-x-3 lg:space-x-0 rtl:space-x-reverse">
+                            <div class="flex space-x-3 lg:order-2 lg:space-x-0 rtl:space-x-reverse">
 
 
-                                <a href="keranjang.php" class="text-white bg-yellow-400 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-1 text-center">
-                                    <i class="fa-solid fa-basket-shopping text-lg rounded-full px-4 text-white "></i>
+                                <a href="keranjang.php" class="px-4 py-1 text-sm font-medium text-center text-white bg-yellow-400 rounded-lg hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300">
+                                    <i class="px-4 text-lg text-white rounded-full fa-solid fa-basket-shopping "></i>
                                 </a>
                             </div>
                         </li>
@@ -82,18 +82,18 @@ $typeKamar = mysqli_query($conn, "SELECT * FROM tipeKamar");
 
     <!-- data -->
     <section>
-        <div class="container mx-auto px-8 md:px-28 p-4">
-            <div class="md:flex flex-row items-center">
+        <div class="container p-4 px-8 mx-auto md:px-28">
+            <div class="flex-row items-center md:flex">
                 <div class="flex-1">
-                    <label class="font-semibold text-sm lg:text-base" for="checkin">checkin</label> <br />
+                    <label class="text-sm font-semibold lg:text-base" for="checkin">checkin</label> <br />
                     <input type="checkin" class="w-[90%] text-sm lg:text-base border border-gray-400 p-2 rounded-lg mr-4" value="<?= date("l, d-m-Y", strtotime($checkin)) ?>">
                 </div>
                 <div class="flex-1">
-                    <label class="font-semibold text-sm lg:text-base" for="checkout">checkout</label> <br />
+                    <label class="text-sm font-semibold lg:text-base" for="checkout">checkout</label> <br />
                     <input type="checkout" class="w-[90%] text-sm lg:text-base border border-gray-400 p-2 rounded-lg mr-4" value="<?= date("l, d-m-Y", strtotime($checkout)) ?>">
                 </div>
                 <div class="flex-1">
-                    <label class="font-semibold text-sm lg:text-base" for="jumlah tamu">Jumlah Tamu</label> <br />
+                    <label class="text-sm font-semibold lg:text-base" for="jumlah tamu">Jumlah Tamu</label> <br />
                     <input type="number" class="w-[90%] text-sm lg:text-base border border-gray-400 p-2 rounded-lg mr-4" value="<?= $jmlh ?>">
                 </div>
             </div>
@@ -103,18 +103,18 @@ $typeKamar = mysqli_query($conn, "SELECT * FROM tipeKamar");
     <!-- Room -->
     <?php while ($rowType = mysqli_fetch_assoc($typeKamar)) : ?>
         <section>
-            <div class="container mx-auto px-8 py-10 md:py-0 md:px-28">
-                <h1 class="text-xl font-bold mb-2 lg:text-2xl"><?= $rowType['nama_tipe'] ?></h1>
+            <div class="container px-8 py-10 mx-auto md:py-0 md:px-28">
+                <h1 class="mb-2 text-xl font-bold lg:text-2xl"><?= $rowType['nama_tipe'] ?></h1>
                 <hr class="mb-2">
-                <div class="md:flex flex-row ">
+                <div class="flex-row md:flex ">
                     <div class="basis-1/4 ">
-                        <div class="pb-10 md:pb-28 mb-5 max-w-60 bg-white border border-gray-300 rounded-lg shadow ">
+                        <div class="pb-10 mb-5 bg-white border border-gray-300 rounded-lg shadow md:pb-28 max-w-60 ">
                             <a href="#">
-                                <img class="rounded-t-lg w-full" style="max-width: 500px;" src="../assets/images/<?= $rowType['image'] ?>.jpeg" alt="" />
+                                <img class="w-full rounded-t-lg" style="max-width: 500px;" src="../assets/images/<?= $rowType['image'] ?>.jpeg" alt="" />
                             </a>
                             <div class="p-4">
-                                <i class="fa-solid fa-user text-gray-800 text-base lg:text-lg"></i><span class="text-sm lg:text-base"> Untuk <?= $rowType['jmlh_tamu'] ?> Orang</span> <br>
-                                <i class="fa-solid fa-door-open text-gray-800 text-base lg:text-lg"></i><span class="text-sm lg:text-base"> <?= $rowType['ukuran'] ?> sqm</span> <br>
+                                <i class="text-base text-gray-800 fa-solid fa-user lg:text-lg"></i><span class="text-sm lg:text-base"> Untuk <?= $rowType['jmlh_tamu'] ?> Orang</span> <br>
+                                <i class="text-base text-gray-800 fa-solid fa-door-open lg:text-lg"></i><span class="text-sm lg:text-base"> <?= $rowType['ukuran'] ?> sqm</span> <br>
                                 <button data-modal-target="<?= $rowType['slug'] ?>" data-modal-toggle="<?= $rowType['slug'] ?>" class="font-medium text-blue-600 hover:underline">Read more &raquo;</button>
                             </div>
                         </div>
@@ -124,26 +124,26 @@ $typeKamar = mysqli_query($conn, "SELECT * FROM tipeKamar");
                     <?php include "modal/detail.php"; ?>
 
                     <div class="flex-1 ">
-                        <div class="mb-5 max-w-full p-6 bg-white border-l-2 border-r-2 border-gray-200 rounded-lg shadow  flex flex-col justify-between">
-                            <div class="flex justify-between items-center">
+                        <div class="flex flex-col justify-between max-w-full p-6 mb-5 bg-white border-l-2 border-r-2 border-gray-200 rounded-lg shadow">
+                            <div class="flex items-center justify-between">
                                 <div class="w-1/2">
                                     <a href="#">
                                         <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 ">Room</h5>
                                     </a>
-                                    <i class="fa-solid fa-check text-gray-800 text-sm lg:text-lg"></i><span class="text-sm lg:text-base"> Tidak Dapat Dikembalikan</span>
-                                    <i class="fa-solid fa-check text-gray-800 text-sm lg:text-lg"></i><span class="text-sm lg:text-base"> Tidak Dapat Dijadwal Ulang</span> <br>
-                                    <i class="fa-solid fa-check text-gray-800 text-sm lg:text-lg"></i><span class="text-sm lg:text-base text-yellow-500"> Termasuk Sarapan (jika memilih fasilitas tambahan sarapan)</span> <br><br>
+                                    <i class="text-sm text-gray-800 fa-solid fa-check lg:text-lg"></i><span class="text-sm lg:text-base"> Tidak Dapat Dikembalikan</span>
+                                    <i class="text-sm text-gray-800 fa-solid fa-check lg:text-lg"></i><span class="text-sm lg:text-base"> Tidak Dapat Dijadwal Ulang</span> <br>
+                                    <i class="text-sm text-gray-800 fa-solid fa-check lg:text-lg"></i><span class="text-sm text-yellow-500 lg:text-base"> Termasuk Sarapan (jika memilih fasilitas tambahan sarapan)</span> <br><br>
 
-                                    <span class="text-lg lg:text-xl font-bold">Pembayaran</span><br>
-                                    <i class="fa-solid fa-check text-gray-800 text-sm lg:text-lg"></i><span class="text-sm lg:text-base"> Bayar Sekarang</span>
-                                    <i class="fa-solid fa-check text-gray-800 text-sm lg:text-lg"></i><span class="text-sm lg:text-base"> Bayar Nanti</span> <br>
+                                    <span class="text-lg font-bold lg:text-xl">Pembayaran</span><br>
+                                    <i class="text-sm text-gray-800 fa-solid fa-check lg:text-lg"></i><span class="text-sm lg:text-base"> Bayar Sekarang</span>
+                                    <i class="text-sm text-gray-800 fa-solid fa-check lg:text-lg"></i><span class="text-sm lg:text-base"> Bayar Nanti</span> <br>
                                 </div>
                                 <div class="text-right">
                                     <span class="text-xs lg:text-sm"> *Harga Untuk Satu Malam</span> <br>
-                                    <span class="text-xl lg:text-2xl font-semibold"> RP <?= number_format($rowType['harga'], 0, ',', '.') ?></span> <br>
+                                    <span class="text-xl font-semibold lg:text-2xl"> RP <?= number_format($rowType['harga'], 0, ',', '.') ?></span> <br>
                                     <span class="text-xs lg:text-xs"> sudah termasuk pajak</span><br>
 
-                                    <button data-modal-target="<?= "konfirmasi-" . $rowType['slug'] ?>" data-modal-toggle="<?= "konfirmasi-" . $rowType['slug'] ?>" class="mt-10 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    <button data-modal-target="<?= "konfirmasi-" . $rowType['slug'] ?>" data-modal-toggle="<?= "konfirmasi-" . $rowType['slug'] ?>" class="inline-flex items-center px-3 py-2 mt-10 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         Pilih Kamar
                                     </button>
 
@@ -162,13 +162,13 @@ $typeKamar = mysqli_query($conn, "SELECT * FROM tipeKamar");
 
 
     <!-- home -->
-    <section class="home bg-yellow-400 w-fit p-3 rounded-2xl fixed bottom-5 right-5 opacity-30 hover:opacity-100 z-50">
+    <section class="fixed z-50 p-3 bg-yellow-400 home w-fit rounded-2xl bottom-5 right-5 opacity-30 hover:opacity-100">
         <a href="../index.php">
             <svg role="img" viewBox="0 0 24 24" width="30px" height="20px" class="m-auto" xmlns="http://www.w3.org/2000/svg">
                 <title>Google Home</title>
                 <path d="M12 0a1.44 1.44 0 0 0-.947.399L.547 10.762a1.26 1.26 0 0 0-.342.808v11.138c0 .768.53 1.292 1.311 1.292h20.968c.78 0 1.311-.522 1.311-1.292V11.57a1.25 1.25 0 0 0-.34-.804L15.68 3.097h-.001L12.947.4A1.454 1.454 0 0 0 12 0Zm0 6.727 6.552 6.456v5.65H5.446v-5.65z" />
             </svg>
-            <p class="text-center text-xs font-semibold">Home</p>
+            <p class="text-xs font-semibold text-center">Home</p>
         </a>
     </section>
 
